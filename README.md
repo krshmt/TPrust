c2b92f524f8ddee8d046b7b17fdbebbabc2d7bc9
kris-toure <goliathheat@gmail.com>
2025-01-16 10:27:47 +0100

    + Ajout du code pour passer une image en monochrome
    + Ajout du CHANGELOG pour dater nos modifications du projet
    
    ~ Ajout du contenu dans le README pour montrer comment exécuter le code
    
    ~ Modification de l'architecture pour compiler

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# TPrust
-Tp noté en rust. Objectif de ce TP, faire des transformations d'images (comme des filtres)
+# Kris TOURE et Rémi BOULAY
+## TP Rust - 2025
+
+1. Objectif du projet
+
+    Réaliser quelques manipulations d’images à l’aide de la bibliothèque rust image
+
+2. Exécuter nos codes
+
+    Pour faire "Passage en monochrome par seuillage" : 
+
+    ```bash
+    cd src/
+    cargo build
+    cargo run
+    ```
+
+    Après l'éxecution du code, l'image doit se trouver dans le fichier src/images/IUT_seuillage.png 
--- src/seuillage.rs
+++ src/seuillage.rs
@@ -8,12 +8,14 @@ fn main() {
     let mut img_buffer = ImageBuffer::new(width, height);
 
     for (x, y, pixel) in img.pixels() {
-        if (x + y) % 2 == 0 {
-            img_buffer.put_pixel(x, y, Rgba([255, 255, 255, 255]));
+        let Rgba(data) = pixel;
+        let luminance = 0.299 * data[0] as f32 + 0.587 * data[1] as f32 + 0.114 * data[2] as f32;
+        if luminance > 128.0 {
+            img_buffer.put_pixel(x, y, Rgba([255u8, 255u8, 255u8, 255u8]));
         } else {
-            img_buffer.put_pixel(x, y, pixel);
+            img_buffer.put_pixel(x, y, Rgba([0u8, 0u8, 0u8, 255u8]));
         }
     }
 
-    img_buffer.save("images/image_modifiee.png").expect("Failed to save image");
+    img_buffer.save("images/IUT_seuillage.png").expect("Failed to save image");
 }
